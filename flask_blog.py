bcef4010cccced9d1eac20a4f78a8f3e9f71a355
Mario Noseda <mnoseda@bluewin.ch>
2020-02-02 16:40:22 +0100

    titles and bodies using jinja2 templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+posts = [
+	{
+		"author": "Mario Noseda",
+		"title": "Blog Post 1",
+		"content": "First post content",
+		"date_posted": "February 02, 2020"
+	},
+	{
+		"author": "Celina Steffani",
+		"title": "Blog Post 2",
+		"content": "Second post content",
+		"date_posted": "February 03, 2020"
+	}
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1>Hello, World!</h1>"
+	return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-	return "About page"
+	return render_template("about.html", title="About")
 
 
 
